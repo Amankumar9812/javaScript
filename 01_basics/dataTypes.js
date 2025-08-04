f90f0a968f8d7772a0d85255706349d5b88f1560
Aman Kumar <amankumar981231@gmail.com>
2025-08-04 17:40:30 +0000

    data type summary

--- 01_basics/dataTypes.js
+++ 01_basics/dataTypes.js
@@ -20,9 +20,9 @@ const myObj={
 
 const heroes=["a","b","c"]
 
-console.log(typeof bigInteger)
-console.log(typeof myFunction)
-console.log(typeof myObj)
+console.log(typeof bigInteger);
+console.log(typeof myFunction);
+console.log(typeof myObj);
 console.log(typeof number);
 console.log(typeof heroes); //Because in JavaScript, arrays are objects — they are a special kind of object designed to hold ordered data.But under the hood, they are still implemented as objects with keys and values.
 console.log(typeof heroes[1]);
