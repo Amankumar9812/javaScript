3761d150637da5b6dbfcd74b91daf5ce12234f79
Aman Kumar <amankumar981231@gmail.com>
2025-08-04 14:18:07 +0000

    operation commit

---
+++ 01_basics/conversionoperation.js
@@ -0,0 +1,10 @@
+let value=3
+let negValue= -value
+console.log("1"+2);
+console.log(1+"2");
+console.log("1"+2+2); //string first conversion as string
+console.log(1+2+"2"); //number first conversion as number
+//but in real life never use such practice or expresion for coding
+
+
+
